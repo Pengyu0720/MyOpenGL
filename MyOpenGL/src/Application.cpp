9c31a97f479e3dd7dc9b6bd6245168a0c634449f
Pengyu0720 <pengyu_720@outlook.com>
2022-08-20 19:34:54 +0800

    barbara

--- MyOpenGL/src/Application.cpp
+++ MyOpenGL/src/Application.cpp
@@ -1,42 +1,117 @@
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "VertexBuffer.h"
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+#include "Renderer.h"
+#include "Texture.h"
+
+GLFWwindow* InitWindow()
+{
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "MyOpenGL", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return nullptr;
+
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return nullptr;
+    }
+
+    std::cout << "Using GL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    return window;
+}
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = InitWindow();
     if (!window)
-    {
-        glfwTerminate();
         return -1;
-    }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    float positions[] = {
+        -0.5f, -0.5f, 0.0f, 0.0f, // 0
+         0.5f, -0.5f, 1.0f, 0.0f, // 1
+         0.5f,  0.5f, 1.0f, 1.0f, // 2
+        -0.5f,  0.5f, 0.0f, 1.0f  // 3
+    };
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        IndexBuffer ib(indices, 6);
 
-        glBegin(GL_TRIANGLES);
-            glVertex2f(-0.5f, -0.5);
-            glVertex2f( 0.0f,  0.5);
-            glVertex2f( 0.5f, -0.5);
-        glEnd();
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        VertexBufferLayout layout;
+        layout.AddFloat(2);
+        layout.AddFloat(2);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        va.AddBuffer(vb, layout);
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+
+        Texture texture("res/textures/barbara.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
+
+        Renderer renderer;
+
+        do {
+            renderer.Clear();
+            renderer.Draw(va, ib, shader);
+
+            // Swap buffers
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        } // Check if the ESC key was pressed or the window was closed
+        while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+            glfwWindowShouldClose(window) == 0);
     }
 
+    // Close OpenGL window and terminate GLFW
     glfwTerminate();
+
     return 0;
 }
